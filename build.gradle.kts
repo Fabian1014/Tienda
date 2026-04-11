bd4aee5c0e0fd7dacab1cb00f36be8d87eb5604f
Fabian Viveros <alcon123@gmail.com>
2026-04-11 11:54:11 -0500

    se agrego funcion para agregar datos a base de datos supabase, al registrarse.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,54 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.application)
+    alias (libs.plugins.kotlin.serialization)
+}
+
+android {
+    namespace = "com.tienda"
+    compileSdk {
+        version = release(36) {
+            minorApiLevel = 1
+        }
+    }
+
+    defaultConfig {
+        applicationId = "com.tienda"
+        minSdk = 31
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation(platform(libs.supabase.bom))
+    implementation(libs.supabase.postgrest)
+    implementation(libs.supabase.auth)
+    implementation(libs.ktor.client.android)
+    implementation(libs.kotlinx.serialization.json)
 }
---
+++ src/main/java/com/tienda/MainActivity.kt
@@ -0,0 +1,66 @@
+package com.tienda
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
+import com.tienda.ui.main.perfil.PerfilFragment
+import com.tienda.ui.main.productos.CarritoFragment
+import com.tienda.ui.main.productos.FavoritosFragment
+import com.tienda.ui.main.productos.HomeFragment
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var drawerLayout: androidx.drawerlayout.widget.DrawerLayout
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        val toolbar = findViewById<androidx.appcompat.widget.Toolbar>(R.id.toolbar)
+        drawerLayout = findViewById(R.id.drawer_layout)
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_nav)
+        val navView = findViewById<NavigationView>(R.id.nav_view)
+        setSupportActionBar(toolbar)
+
+        val toggle = ActionBarDrawerToggle(
+            this,
+            drawerLayout,
+            toolbar,
+            R.string.navigation_drawer_open,
+            R.string.navigation_drawer_close
+        )
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        cargarFragment(HomeFragment())
+        bottomNav.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_home -> cargarFragment(HomeFragment())
+                R.id.nav_Catalogo -> cargarFragment(FavoritosFragment())
+                R.id.nav_carrito -> cargarFragment(CarritoFragment())
+                R.id.nav_favoritos-> cargarFragment(PerfilFragment())
+            }
+            true
+        }
+        navView.setNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_Pedidos -> cargarFragment(HomeFragment())
+                R.id.nav_notificaciones -> cargarFragment(FavoritosFragment())
+                R.id.pago -> cargarFragment(CarritoFragment())
+                R.id.nav_sesion -> cargarFragment(PerfilFragment())
+            }
+            drawerLayout.closeDrawers()
+            true
+            }
+    }
+    private fun cargarFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .commit()
+    }
+}
